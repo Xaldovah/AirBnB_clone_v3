2cf94270685c8b1c0e6cdccc2736e9754adce08b
Dennis Kimani <dxaldovah@gmail.com>
2023-10-30 06:02:59 -0700

    Resolved merge conflicts

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-=======
 """This module handles all the initializations"""
->>>>>>> storage_get_count
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
@@ -11,7 +8,4 @@ from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
-<<<<<<< HEAD
 from api.v1.views.users import *
-=======
->>>>>>> storage_get_count
